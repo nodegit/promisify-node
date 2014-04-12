3a72e0fb081ae32cdf52878cd9422e7771be2662
Mike Pennisi <mike@mikepennisi.com>
2014-04-11 21:13:10 -0400

    Update test to use Mocha's promise handling

--- test/index.js
+++ test/index.js
@@ -2,16 +2,15 @@ var promisify = require("../");
 var assert = require("assert");
 
 describe("Promisify", function() {
-  it("can convert a basic async function", function(done) {
+  it("can convert a basic async function", function() {
     function test(cb) {
       cb(null, true);
     }
 
     var wrappedTest = promisify(test);
-    
-    wrappedTest().then(function(value) {
+
+    return wrappedTest().then(function(value) {
       assert.ok(value);
-      done();
     });
   });
 
