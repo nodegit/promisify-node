59134321c984f27983f07817f1f5f9aeab9e597b
Mike Pennisi <mike@mikepennisi.com>
2014-04-11 21:13:51 -0400

    Add tests for asynchronous method inference

--- test/index.js
+++ test/index.js
@@ -17,4 +17,54 @@ describe("Promisify", function() {
   it("exports a callbacks array", function() {
     assert(Array.isArray(promisify.callbacks));
   });
+
+  describe("asynchronous method inference", function() {
+    var later = function(cb) {
+      setTimeout(cb(null), 0);
+    };
+
+    it("does not modify methods that do not appear to be asynchronous", function() {
+      var obj = {
+        a: function(probably, not, async) {}
+      };
+      var wrappedObj = promisify(obj);
+
+      assert.equal(
+        obj.a,
+        wrappedObj.a
+      );
+    });
+
+    it("can infer callback-accepting functions by argument list", function() {
+      var obj = promisify({
+        a: function(cb) { later(cb); }
+      });
+
+      return obj.a();
+    });
+
+    it("can infer callback-accepting functions by argument list", function() {
+      var obj = promisify({
+        a: function(callback) { later(callback); }
+      });
+
+      return obj.a();
+    });
+
+    it("can infer callback-accepting functions by argument list", function() {
+      var obj = promisify({
+        a: function(callback_) { later(callback_); }
+      });
+
+      return obj.a();
+    });
+
+    it("can infer callback-accepting functions by argument list", function() {
+      var obj = promisify({
+        a: function(done) { later(done); }
+      });
+
+      return obj.a();
+    });
+  });
 });
