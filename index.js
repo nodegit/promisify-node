368682489bb630977f6732c7df95562f6afa7102
Tim Branyen <tim@tabdeveloper.com>
2014-04-11 20:25:57 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,73 @@
+const Promise = require("promise");
+const args = require("./utils/args");
+
+// Unfortunately this list is not exhaustive, so if you find that a method does
+// not use a "standard"-ish name, you'll have to extend this list.
+var callbacks = exports.callbacks = ["cb", "callback", "callback_", "done"];
+
+/**
+ * Recursively operate over an object locating "asynchronous" functions by
+ * inspecting the last argument in the parameter signature for a callback.
+ *
+ * @param {*} exports - Should be a function or an object, identity other.
+ * @returns {*} exports - Identity.
+ */
+function processExports(exports) {
+  // Pass through if not an object or function.
+  if (typeof exports != "object" && typeof exports != "function") {
+    return exports;
+  }
+
+  // If a function, simply return it wrapped.
+  if (typeof exports === "function") {
+    return Promise.denodeify(exports);
+  }
+
+  Object.keys(exports).map(function(keyName) {
+    // Convert to values.
+    return [keyName, exports[keyName]];
+  }).filter(function(keyVal) {
+    var value = keyVal[1];
+
+    // If an object is encountered, recursively traverse.
+    if (typeof value === "object") {
+      processExports(exports);
+    }
+    // Filter to functions with callbacks only.
+    else if (typeof value === "function") {
+      // If the callback name exists as the last argument, consider it an
+      // asynchronous function.  Brittle? Fragile? Effective.
+      if (callbacks.indexOf(args(value).slice(-1)[0]) > -1) {
+        return true;
+      }
+    }
+  }).forEach(function(keyVal) {
+    var keyName = keyVal[0];
+    var func = keyVal[1];
+
+    // Assign the new function in place.
+    exports[keyName] = Promise.denodeify(func);
+  });
+
+  return exports;
+}
+
+/**
+ * Public API for Promisify.  Will resolve modules names using `require`.
+ *
+ * @param {*} name - Can be a module name, object, or function.
+ * @returns {*} exports - The resolved value from require or passed in value.
+ */
+module.exports = function(name) {
+  var exports = name;
+
+  // If the name argument is a String, will need to resovle using the built in
+  // Node require function.
+  if (typeof name === "string") {
+    exports = require(name);
+  }
+
+  // Iterate over all properties and find asynchronous functions to convert to
+  // promises.
+  return processExports(exports);
+};
---
+++ test/index.js
@@ -0,0 +1,17 @@
+var promisify = require("../");
+var assert = require("assert");
+
+describe("Promisify", function() {
+  it("can convert a basic async function", function(done) {
+    function test(cb) {
+      cb(null, true);
+    }
+
+    var wrappedTest = promisify(test);
+    
+    wrappedTest().then(function(value) {
+      assert.ok(value);
+      done();
+    });
+  });
+});
